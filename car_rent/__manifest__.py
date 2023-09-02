d6fb3919e499cd5ce5ae9968a5ed2bb23d00e2dc
Luka <lukagoderdzishvili4@gmail.com>
2023-09-02 13:53:46 +0400

    Create xml file for res.partner model

--- car_rent/__manifest__.py
+++ car_rent/__manifest__.py
@@ -5,12 +5,14 @@
     'data':[
         'security/security.xml',
         'security/ir.model.access.csv',
+        'data/car_rent_car_tag_data.xml',
+        'data/car_rent_car_data.xml',
         'views/car_rent_car_views.xml',
         'views/car_rent_driver_views.xml',
         'views/car_rent_tag_views.xml',
+        'views/res_partner_views.xml',
         'views/car_rent_menus.xml',
-        'data/car_rent_car_tag_data.xml',
-        'data/car_rent_car_data.xml'
+        
     ],
     'icon': 'static/description/icon_image.png',
     'installable': True,
