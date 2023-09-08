f9c5e984aef9ac5b3a63daa6daede9a9f4d2d480
Luka <lukagoderdzishvili4@gmail.com>
2023-09-08 17:31:13 +0400

    Remove comma after readonly=True

--- car_rent/models/car_rent_order.py
+++ car_rent/models/car_rent_order.py
@@ -6,5 +6,5 @@ class CarRentOrder(models.Model):
     _description = 'Car Rent Order'
 
     rent_order_number = fields.Char(default=lambda self: self.env['ir.sequence'].next_by_code('car.rent.order.sequence'))
-    customer_id = fields.Many2one('res.users', default=lambda self: self.env.user, readonly=True,)
+    customer_id = fields.Many2one('res.users', default=lambda self: self.env.user, readonly=True)
     car_id = fields.Many2one('car.rent.car')
