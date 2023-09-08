a8b228e39efd43d83bab2f9dad1c8edbc1fcd39b
Luka <lukagoderdzishvili4@gmail.com>
2023-09-08 16:36:29 +0400

    add orders

---
+++ car_rent/models/car_rent_order.py
@@ -0,0 +1,10 @@
+from odoo import fields,models
+
+
+class CarRentOrder(models.Model):
+    _name = 'car.rent.order'
+    _description = 'Car Rent Order'
+
+    rent_order_number = fields.Char(default=lambda self: self.env['ir.sequence'].next_by_code('car.rent.order.sequence'))
+    customer_id = fields.Many2one('res.users', default=lambda self: self.env.user, readonly=True,)
+    car_id = fields.Many2one('car.rent.car')
