4386564d60dd57dd77b6ebbf3cadb1681ad5e4a0
Luka <lukagoderdzishvili4@gmail.com>
2023-09-09 00:24:59 +0400

    Remove data/car_rent_car_tag_data.xml from manifest

--- car_rent/__manifest__.py
+++ car_rent/__manifest__.py
@@ -5,7 +5,6 @@
     'data':[
         'security/security.xml',
         'security/ir.model.access.csv',
-        'data/car_rent_car_tag_data.xml',
         'data/car_rent_car_data.xml',
         'views/car_rent_car_views.xml',
         'views/car_rent_driver_views.xml',
