417b363ce9aed59b7c85ad9869cebb58a43f8a65
Luka <lukagoderdzishvili4@gmail.com>
2023-09-08 22:44:53 +0400

    Remove required=True from name field in car.category model

--- car_rent/models/car_category.py
+++ car_rent/models/car_category.py
@@ -5,7 +5,7 @@ class CarRentTag(models.Model):
     _name = 'car.category'
     _description = 'Car Category'
 
-    name = fields.Char(string="Category", required=True)
+    name = fields.Char(string="Category")
     
     _sql_constraints = [
         ('unique_name', 'unique(name)', 'Category name must be unique!'),
