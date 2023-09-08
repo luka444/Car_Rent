ade94782cbb322b704b2b918159cd043663e0f60
Luka <lukagoderdzishvili4@gmail.com>
2023-09-09 00:29:11 +0400

    add create method in car.rent.order model and add noupdate="1" in xml file

--- car_rent/models/car_rent_order.py
+++ car_rent/models/car_rent_order.py
@@ -1,10 +1,20 @@
-from odoo import fields,models
+from odoo import fields,models,api
 
 
 class CarRentOrder(models.Model):
     _name = 'car.rent.order'
     _description = 'Car Rent Order'
 
-    rent_order_number = fields.Char(default=lambda self: self.env['ir.sequence'].next_by_code('car.rent.order.sequence'))
     customer_id = fields.Many2one('res.users', default=lambda self: self.env.user, readonly=True)
     car_id = fields.Many2one('car.rent.car')
+    rent_order_number = fields.Char(required=True, copy=False, readonly=True,default=lambda self:('New'))
+
+    @api.model
+    def create(self, vals):
+
+        if vals.get('rent_order_number', ("New")) == ("New"):
+            vals['rent_order_number'] = self.env['ir.sequence'].next_by_code(
+                'car.rent.order.sequence',) or ("New")
+            
+        res = super(CarRentOrder,self).create(vals)
+        return res
